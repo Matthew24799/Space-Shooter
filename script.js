75b49e1eef5db57ed348a6c53a8acce2bbde4229
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-03-05 01:47:49 -0500

    Added Player Square

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const canvas = document.querySelector("#playArea");
 const ctx = canvas.getContext("2d");
 
+    
+const playerColor = ctx.strokeStyle = "red";
+ ctx.stroke();
+ const player = ctx.strokeRect(250, 800, 45, 55);
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+ 
